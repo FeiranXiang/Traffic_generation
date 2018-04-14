46534844548e854fe9c64702119bfd6abec80727
lin <hitwhlin@163.com>
2018-04-14 21:10:43 +0800

    Add dns packets

--- dns_traffic_generation.py
+++ dns_traffic_generation.py
@@ -1,10 +1,13 @@
-import dns.message
-import dns.query
+from scapy.all import *
 
-SERVER = "127.0.0.1"#your DNS server
-PORT = 8000#DNS server port 
-dns_query = dns.message.make_query("www.baidu.com", "A")
-response = dns.query.udp(dns_query, SERVER, port = PORT)
+def dns_packets():
+    a = IP(dst='2.3.4.5',src='5.6.7.8')
+    b = UDP()
+    c = DNS(id=1,qr=0,opcode=0,tc=0,rd=1,qdcount=1,ancount=0,nscount=0,arcount=0)
+    c.qd=DNSQR(qname='www.qq.com',qtype=1,qclass=1)
+    p = a/b/c
+    send(p)
 
-for i in response.answer:
-    print (i.to_text())
+
+if __name__ == "__main__":
+    dns_packets()
